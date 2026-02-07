88aa1d0615d9bd8aac043615946acd5f54946142
Zahid-Jimenez <148026651+Zahid-Jimenez@users.noreply.github.com>
2026-02-07 12:53:37 -0600

    suma hecha
    suma funcional

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -1,3 +1,5 @@
+package com.example.helloworld;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -6,56 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fing.app.models.Product;
-
 @RestController
-public class Calc {
+public class HelloController {
 
-	
-	
-	
-	
-	@PostMapping("/prod") // Maps to POST /api/products
-    public String createProduct(@RequestBody Product product) {
-        
-        System.out.println("Received product: " + product.getName());
-
-        
-        return "Ok";
-    }
-	//hola
-	
 	
 	
 	@RequestMapping("/")
 	public String root() {
 		
-		return "Hola";
+		return "Hola finalmente :')";
 	}
 	
 	
 	
 	@GetMapping("/calc/{oper}/{n1}/{n2}")
-    public String calc(@PathVariable int n1) 
+    public String calc(@PathVariable String oper,@PathVariable int n1,@PathVariable int n2)
 	{
-        
-        return "n1 = " + n1;
+        if("suma".equals(oper)){
+            return "Resultado: " + (n1 + n2);
+        }
+        return "Operación no soportada";
     }
 	
-	
-	
-	@GetMapping("/users/{userId}")
-    public String getUserDetails(@PathVariable Long userId, 
-    		@RequestParam(value = "name", defaultValue = "World") String name) 
-	{
-        // Use the userId to fetch data, e.g., from a database
-        return "User ID: " + userId + ", " + name;
-    }
 	
 	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name, 
-    		            @RequestParam(value = "carrera", defaultValue = "World") String carrera
-    		) {
-        return String.format("Hello %s!, %s", name, carrera);
+    public String hello() {
+        return "Hello ";
     }
 }
